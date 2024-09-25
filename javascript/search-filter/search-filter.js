e0f6cd37aedc14a8eecea41877f27571373270f5
ramGit22 <karki_ram@hotmail.com>
2024-09-25 19:49:31 +0300

    javascript ex.3

--- javascript/search-filter/search-filter.js
+++ javascript/search-filter/search-filter.js
@@ -6,9 +6,18 @@ document.addEventListener('DOMContentLoaded', function () {
   searchInput.addEventListener('keyup', function () {
     const searchInputValue = searchInput.value.toLowerCase()
 
-    for (i = 0; i < li.length; i++) {
-      const listValues = li[i].textContent.toLowerCase()
-      if (listValues.includes(searchInputValue)) {
+    for (let i = 0; i < li.length; i++) {
+      const itemText = li[i].innerText.toLowerCase()
+      const startIndex = itemText.indexOf(searchInputValue)
+
+      if (startIndex !== -1) {
+        const endIndex = startIndex + searchInputValue.length
+        const highlightedText = li[i].innerText.substring(startIndex, endIndex)
+        const newText =
+          li[i].innerText.substring(0, startIndex) +
+          `<span style='background-color:yellow'>${highlightedText}</span>` +
+          li[i].innerText.substring(endIndex)
+        li[i].innerHTML = newText
         li[i].style.display = ''
       } else {
         li[i].style.display = 'none'
