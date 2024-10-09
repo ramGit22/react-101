b2b9bdfd8e0919cede9b9fda0df7d3c477d74038
ramGit22 <karki_ram@hotmail.com>
2024-10-09 19:07:52 +0300

    javascript ex.5

--- javascript/polling-app/polling-app.js
+++ javascript/polling-app/polling-app.js
@@ -1,8 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
+  let votes = JSON.parse(localStorage.getItem('votes')) || {
+    'Option A': 0,
+    'Option B': 0,
+    'Option C': 0,
+  }
+
   const ul = document.getElementById('pollOptions')
-  const btns = ul.getElementsByTagName('button')
+  const btns = ul.getElementsByClassName('voteBtn')
   const pollResults = document.getElementById('pollResults')
-  let votes = { 'Option A': 0, 'Option B': 0, 'Option C': 0 }
 
   function updateList() {
     pollResults.innerHTML = ''
@@ -12,13 +17,16 @@ document.addEventListener('DOMContentLoaded', function () {
       li.className = 'poll-option'
       pollResults.appendChild(li)
     }
+    localStorage.setItem('votes', JSON.stringify(votes))
   }
 
-  for (let item in btns) {
-    btns[item].addEventListener('click', function () {
+  Array.from(btns).forEach((button) => {
+    button.addEventListener('click', function () {
       const option = this.parentElement.querySelector('span').textContent
       votes[option] += 1
       updateList()
     })
-  }
+  })
+
+  updateList()
 })
